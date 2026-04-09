dad0ff29ea95c79e8c0e9a7eea81f34c480745f2
Bharatflows <admin@bharatflows.com>
2026-04-10 02:49:40 +0530

    Fix usePurchase.ts: add all CRUD hooks (CreatePurchaseBill, UpdatePurchaseBill, GRN, DebitNote stubs)

--- client/src/hooks/usePurchase.ts
+++ client/src/hooks/usePurchase.ts
@@ -1,10 +1,18 @@
 export function usePurchaseBills(..._: any[]): any { return { data: [], isLoading: false }; }
 export function useDeletePurchaseBill(..._: any[]): any { return { mutate: () => {} }; }
+export function useCreatePurchaseBill(..._: any[]): any { return { mutate: () => {}, isPending: false }; }
+export function useUpdatePurchaseBill(..._: any[]): any { return { mutate: () => {}, isPending: false }; }
 export const purchaseKeys = { all: ['purchase'] };
 export function usePurchaseOrders(..._: any[]): any { return { data: [], isLoading: false }; }
 export function useDeletePurchaseOrder(..._: any[]): any { return { mutate: () => {} }; }
+export function useCreatePurchaseOrder(..._: any[]): any { return { mutate: () => {}, isPending: false }; }
+export function useUpdatePurchaseOrder(..._: any[]): any { return { mutate: () => {}, isPending: false }; }
 export function useUpdatePurchaseOrderStatus(..._: any[]): any { return { mutate: () => {} }; }
 export function useGRNs(..._: any[]): any { return { data: [], isLoading: false }; }
 export function useDeleteGRN(..._: any[]): any { return { mutate: () => {} }; }
-export function useCreatePurchaseOrder(..._: any[]): any { return { mutate: () => {} }; }
-export function useUpdatePurchaseOrder(..._: any[]): any { return { mutate: () => {} }; }
+export function useCreateGRN(..._: any[]): any { return { mutate: () => {}, isPending: false }; }
+export function useUpdateGRN(..._: any[]): any { return { mutate: () => {}, isPending: false }; }
+export function useDebitNotes(..._: any[]): any { return { data: [], isLoading: false }; }
+export function useDeleteDebitNote(..._: any[]): any { return { mutate: () => {} }; }
+export function useCreateDebitNote(..._: any[]): any { return { mutate: () => {}, isPending: false }; }
+export function useUpdateDebitNote(..._: any[]): any { return { mutate: () => {}, isPending: false }; }
