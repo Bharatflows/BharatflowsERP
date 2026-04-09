c4d4dedbfafaab92ae31e1b72fd3bb869d66cffe
Bharatflows <admin@bharatflows.com>
2026-04-09 22:41:12 +0530

    Fix usePurchase.ts: add useGRNs, useDeleteGRN, useCreatePurchaseOrder, useUpdatePurchaseOrder

--- client/src/hooks/usePurchase.ts
+++ client/src/hooks/usePurchase.ts
@@ -4,3 +4,7 @@ export const purchaseKeys = { all: ['purchase'] };
 export function usePurchaseOrders(..._: any[]): any { return { data: [], isLoading: false }; }
 export function useDeletePurchaseOrder(..._: any[]): any { return { mutate: () => {} }; }
 export function useUpdatePurchaseOrderStatus(..._: any[]): any { return { mutate: () => {} }; }
+export function useGRNs(..._: any[]): any { return { data: [], isLoading: false }; }
+export function useDeleteGRN(..._: any[]): any { return { mutate: () => {} }; }
+export function useCreatePurchaseOrder(..._: any[]): any { return { mutate: () => {} }; }
+export function useUpdatePurchaseOrder(..._: any[]): any { return { mutate: () => {} }; }
