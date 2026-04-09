272774a48df73ab472334b77fe8900688c8bf6e0
Bharatflows <admin@bharatflows.com>
2026-04-09 22:35:05 +0530

    Fix usePurchase.ts: add usePurchaseOrders, useDeletePurchaseOrder, useUpdatePurchaseOrderStatus

--- client/src/hooks/usePurchase.ts
+++ client/src/hooks/usePurchase.ts
@@ -1,3 +1,6 @@
 export function usePurchaseBills(..._: any[]): any { return { data: [], isLoading: false }; }
 export function useDeletePurchaseBill(..._: any[]): any { return { mutate: () => {} }; }
 export const purchaseKeys = { all: ['purchase'] };
+export function usePurchaseOrders(..._: any[]): any { return { data: [], isLoading: false }; }
+export function useDeletePurchaseOrder(..._: any[]): any { return { mutate: () => {} }; }
+export function useUpdatePurchaseOrderStatus(..._: any[]): any { return { mutate: () => {} }; }
